2c13086b841b7b02f0a5f800ed807e17d86326ba
Aryan Shams <aryan.shamsinejad@gmail.com>
2024-05-22 16:01:50 +0330

    st fish config

---
+++ config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting 
+  echo (set_color green;)Wake up, Neo...
+  echo The Matrix has you...
+end
